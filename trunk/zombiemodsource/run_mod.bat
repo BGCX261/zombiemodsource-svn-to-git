5bb7b90e680cf3151321cbb4ec0868023ed59306
Your Name <you@example.com>
2015-08-25 21:24:21 +0600

    init

---
+++ trunk/zombiemodsource/run_mod.bat
@@ -0,0 +1,1 @@
+@"c:\program files\steam\steamapps\madkowa\half-life 2 deathmatch\hl2.exe" -dev -game "c:\program files\steam\steamapps\SourceMods\zombiemodsource" -allowdebug %1 %2 %3 %4 %5 %6 %7 %8 %9
---
+++ trunk/zombiemodsource/run_studiomdl.bat
@@ -0,0 +1,2 @@
+@"c:\program files\steam\steamapps\madkowa\sourcesdk\bin\studiomdl.exe" -game "c:\program files\steam\steamapps\SourceMods\zombiemodsource" %1 %2 %3 %4 %5 %6 %7 %8 %9
+
